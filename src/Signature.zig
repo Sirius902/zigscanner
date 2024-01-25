470aa12124e1d632230af43765491e466b25ecf0
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2024-01-25 15:22:19 -0800

    Add scan

--- src/Signature.zig
+++ src/Signature.zig
@@ -1,11 +1,18 @@
 const std = @import("std");
 const log = @import("log.zig");
 const testing = std.testing;
+const fmt = @import("fmt.zig");
 const Allocator = std.mem.Allocator;
 
 pattern: []const PatternByte,
 
 const Self = @This();
+
+const suggested_vec_len = std.simd.suggestVectorLength(u8);
+
+/// Scans for a signature within `data`. Returns the index of the start of the
+/// signature.
+pub const scan = if (suggested_vec_len) |_| simdScan else linearScan;
 
 pub fn parse(comptime pattern: []const u8) Self {
     if (!@inComptime()) @compileError("Expected parse to be called in comptime");
@@ -58,6 +65,112 @@ pub fn deinit(self: Self, allocator: Allocator) void {
     allocator.free(self.pattern);
 }
 
+/// Scans for a signature in the range [`begin`, `end`). Returns a pointer to
+/// the start of the signature.
+pub fn scanPtr(self: Self, begin: anytype, end: @TypeOf(begin)) ?@TypeOf(begin) {
+    if (@typeInfo(@TypeOf(begin)).Pointer.child != u8) {
+        @compileError("Expected u8 pointer");
+    }
+
+    const len = @intFromPtr(end) - @intFromPtr(begin);
+    const off = self.scan(begin[0..len]) orelse return null;
+    return @as(@TypeOf(begin), @ptrFromInt(@intFromPtr(begin) + off));
+}
+
+/// Scans linearly for the signature within `data`. Returns the index of the
+/// start of the signature.
+///
+/// Prefer using `scan` instead as it will use an implementation suited to the
+/// target CPU.
+pub fn linearScan(self: Self, data: []const u8) ?usize {
+    var i: usize = 0;
+    while (i < data.len) : (i += 1) {
+        if (self.isMatch(data[i..])) return i;
+    }
+
+    return null;
+}
+
+pub fn isMatch(self: Self, bytes: []const u8) bool {
+    if (bytes.len < self.pattern.len) return false;
+
+    for (self.pattern, 0..) |pb, i| {
+        if (!pb.isMatch(bytes[i])) return false;
+    }
+
+    return true;
+}
+
+fn simdScan(self: Self, data: []const u8) ?usize {
+    const chunk_len = suggested_vec_len orelse unreachable;
+
+    const true_vec: @Vector(chunk_len, bool) = @splat(true);
+    var pattern_vec: @Vector(chunk_len, u8) = @splat(0);
+    var any_select: @Vector(chunk_len, bool) = @splat(true);
+
+    for (self.pattern, 0..) |pb, i| {
+        switch (pb) {
+            .Byte => |b| {
+                pattern_vec[i] = b;
+                any_select[i] = false;
+            },
+            else => {},
+        }
+    }
+
+    var anchor_byte: u8 = undefined;
+    var anchor_off: usize = undefined;
+    for (self.pattern, 0..) |pb, i| {
+        switch (pb) {
+            .Byte => |b| {
+                anchor_byte = b;
+                anchor_off = i;
+                break;
+            },
+            else => {},
+        }
+    } else {
+        std.debug.panic(
+            "Expected pattern to contain at least one Byte, but got: {X:0>2}",
+            .{fmt.fmtSignature(self)},
+        );
+    }
+
+    const anchor_vec: @Vector(chunk_len, u8) = @splat(anchor_byte);
+    const false_vec: @Vector(chunk_len, bool) = @splat(false);
+
+    var chunk: @Vector(chunk_len, u8) = undefined;
+    var i: usize = 0;
+    while (i + chunk_len <= data.len) {
+        @memcpy(@as([*]u8, @ptrCast(&chunk)), data[i .. i + chunk_len]);
+
+        const is_match = @reduce(.And, @select(bool, any_select, true_vec, chunk == pattern_vec));
+        if (is_match) return i;
+
+        var anchors = chunk == anchor_vec;
+        anchors[0] = false;
+
+        var anchor_in_bounds: @Vector(chunk_len, bool) = @splat(true);
+        if (i < anchor_off) {
+            for (0..anchor_off - i) |j| {
+                anchor_in_bounds[j] = false;
+            }
+        }
+
+        const valid_anchors = @select(bool, anchor_in_bounds, anchors, false_vec);
+
+        // TODO: Make sure candidate's start isn't before the start of data.
+        if (std.simd.firstTrue(valid_anchors)) |anchor_idx| {
+            i += anchor_idx - anchor_off;
+            continue;
+        } else {
+            i += chunk_len;
+        }
+    }
+
+    return self.linearScan(data[i..]);
+}
+
 pub const ParseError = error{
     InvalidPattern,
 };
@@ -70,6 +183,13 @@ pub const PatternByte = union(enum) {
         if (str.len != 2) return error.InvalidPattern;
         if (std.mem.eql(u8, str, "??")) return .Any;
         return .{ .Byte = std.fmt.parseInt(u8, str, 16) catch return error.InvalidPattern };
+    }
+
+    pub fn isMatch(self: PatternByte, b: u8) bool {
+        return switch (self) {
+            .Any => true,
+            .Byte => |bb| bb == b,
+        };
     }
 };
 
